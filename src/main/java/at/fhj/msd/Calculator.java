e6488d5ba26e84638051fa5837e3756e0083f3d9
Mihajlo Ilijic <mihajlo.ilijic@edu.fh-joanneum.at>
2024-05-02 10:55:38 +0200

    src

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -2,6 +2,22 @@ package at.fhj.msd;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+    public double add(double number1, double number2){
+
+        double result = number1+number2;
+
+        return result;
+
+    }
+    public double minus(double number1, double number2){
+
+    }
+    public double multiply(double number1, double number2){
+
+    }
+    public double divide(double number1, double number2){
+
     }
 }
